8ec114613a014add8223058f110e380f1bde6790
laptop nerizxn <krotikrig@gmail.com>
2023-03-28 12:54:24 +0300

    a few adjustments

--- main.py
+++ main.py
@@ -4,89 +4,89 @@ from playsound import playsound
 from paho.mqtt.publish import single
 
 
-def read_qr_code(image):
+def read_qr_code(shot):
     try:
         detect = cv2.QRCodeDetector()
-        value, points, straight_qrcode = detect.detectAndDecode(image)
+        value, points, straight_qrcode = detect.detectAndDecode(shot)
         return value
     except:
         return
 
 
 def create_connection(path):
-    connection = None
+    connect = None
     try:
-        connection = sqlite3.connect(path)
+        connect = sqlite3.connect(path)
         print("Подключение к базе данных SQLite прошло успешно")
     except sqlite3.Error as e:
         print(f'Произошла ошибка {e}')
-    return connection
+    return connect
 
 
-def initial_account(account):
-    current_account = (int(account), 0)
-    cur.execute("INSERT or IGNORE into users VALUES (?, ?);", current_account)
+def initial_account(account_id):
+    current_account = (int(account_id), 0)
+    cursor.execute("INSERT or IGNORE into users VALUES (?, ?);", current_account)
     connection.commit()
 
 
-def carton(account):
+def carton(account_id):
     global song
     cv2.putText(image, "Ваш мусор идет в урну для картона.", (125, 200), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 255, 255),
                 1)
     cv2.putText(image, "Она находится по середине.", (150, 290), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 255, 255), 1)
-    cur.execute("SELECT * FROM users WHERE userid = ?", (account, ))
-    records = cur.fetchone()
+    cursor.execute("SELECT * FROM users WHERE userid = ?", (account_id,))
+    records = cursor.fetchone()
     records = records[1] + 1
-    update_account = (account, records)
-    cur.execute("INSERT or REPLACE into users VALUES (?, ?);", update_account)
+    update_account = (account_id, records)
+    cursor.execute("INSERT or REPLACE into users VALUES (?, ?);", update_account)
     connection.commit()
     single("sortingtrash/main",
-           payload=f'На аккаунт {account} был зачислен 1 балл. Баланс:{records}',
+           payload=f'На аккаунт {account_id} был зачислен 1 балл. Баланс:{records}',
            hostname='mqtt.pi40.ru',
            port=1883,
            client_id='python_qw21',
-           auth={'username':'sortingtrash','password':'e122333'})
+           auth={'username': 'sortingtrash', 'password': 'e122333'})
     song = "carton.wav"
 
 
-def plastic(account):
+def plastic(account_id):
     global song
     cv2.putText(image, "Ваш мусор идет в урну для пластика.", (125, 200), cv2.FONT_HERSHEY_COMPLEX, 0.6,
                 (255, 255, 255), 1)
     cv2.putText(image, "Она находится справа от Вас.", (150, 290), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 255, 255),
                 1)
-    cur.execute("SELECT * FROM users WHERE userid = ?", (account, ))
-    records = cur.fetchone()
+    cursor.execute("SELECT * FROM users WHERE userid = ?", (account_id,))
+    records = cursor.fetchone()
     records = records[1] + 2
-    update_account = (account, records)
-    cur.execute("INSERT or REPLACE into users VALUES (?, ?);", update_account)
+    update_account = (account_id, records)
+    cursor.execute("INSERT or REPLACE into users VALUES (?, ?);", update_account)
     connection.commit()
     single("sortingtrash/main",
-           payload=f'На аккаунт {account} было зачислено 2 балла. Баланс:{records}',
+           payload=f'На аккаунт {account_id} было зачислено 2 балла. Баланс:{records}',
            hostname='mqtt.pi40.ru',
            port=1883,
            client_id='python_qw21',
-           auth={'username':'sortingtrash','password':'e122333'})
+           auth={'username': 'sortingtrash', 'password': 'e122333'})
     song = "plastic.wav"
 
 
-def glass(account):
+def glass(account_id):
     global song
     cv2.putText(image, "Ваш мусор идет в урну для стекла.", (125, 200), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 255, 255),
                 1)
     cv2.putText(image, "Она находится слева от Вас.", (150, 290), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 255, 255), 1)
-    cur.execute("SELECT * FROM users WHERE userid = ?", (account, ))
-    records = cur.fetchone()
+    cursor.execute("SELECT * FROM users WHERE userid = ?", (account_id,))
+    records = cursor.fetchone()
     records = records[1] + 3
-    update_account = (account, records)
-    cur.execute("INSERT or REPLACE into users VALUES (?, ?);", update_account)
+    update_account = (account_id, records)
+    cursor.execute("INSERT or REPLACE into users VALUES (?, ?);", update_account)
     connection.commit()
     single("sortingtrash/main",
-           payload=f'На аккаунт {account} было зачислено 3 балла. Баланс:{records}',
+           payload=f'На аккаунт {account_id} было зачислено 3 балла. Баланс:{records}',
            hostname='mqtt.pi40.ru',
            port=1883,
            client_id='python_qw21',
-           auth={'username':'sortingtrash','password':'e122333'})
+           auth={'username': 'sortingtrash', 'password': 'e122333'})
     song = "glass.wav"
 
 
@@ -94,9 +94,10 @@ cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 key = -1
 song = ""
 loginned = False
+loginned_account = None
 connection = create_connection('accounts.db')
-cur = connection.cursor()
-cur.execute("""CREATE TABLE IF NOT EXISTS users(
+cursor = connection.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS users(
    userid INT PRIMARY KEY,
    points INT);""")
 connection.commit()
@@ -110,12 +111,12 @@ while key == -1:
         loginned = True
         loginned_account = account
     if loginned:
-        b, g, r = image[240, 320]
-        if 130 < b < 170 and 180 < g < 210 and 180 < r < 200:
+        blue, green, red = image[240, 320]
+        if 130 < blue < 170 and 180 < green < 210 and 180 < red < 200:
             carton(loginned_account)
-        if b < 40 and g < 40 and r > 150:
+        if blue < 40 and green < 40 and red > 150:
             plastic(loginned_account)
-        if b < 60 and 50 < g < 90 and r < 40:
+        if blue < 60 and 50 < green < 90 and red < 40:
             glass(loginned_account)
     else:
         cv2.putText(image, "Для начала Вам нужно войти.", (170, 200), cv2.FONT_HERSHEY_COMPLEX, 0.6, (20, 20, 255), 1)
@@ -127,6 +128,6 @@ while key == -1:
         except:
             print("Ошибка проигрывания звука", end="\t")
 cap.release()
-cur.execute("SELECT * FROM users;")
-all_results = cur.fetchall()
+cursor.execute("SELECT * FROM users;")
+all_results = cursor.fetchall()
 print(all_results)
